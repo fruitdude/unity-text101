5f8af88a7c0ba6191f79fa8e77d5c155fd811b94
fruitdude <stevenrogge95@gmail.com>
2021-02-09 11:45:51 +0100

    added files

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AdventureGame : MonoBehaviour{
+    [SerializeField] TextMeshProUGUI storyTitleComponent;
+    [SerializeField] TextMeshProUGUI storyTextComponent;
+    [SerializeField] State startingState;
+    
+
+    State state;
+    // Start is called before the first frame update
+    void Start(){
+        state = startingState;
+        printStory();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        ManageState();
+    }
+
+    private void ManageState(){
+        var nextStates = state.GetNextStates();
+        //the foor loop checks how big the array 'nextStates' is
+        //and matches it to the possible answers.
+        for(int index = 0; index < nextStates.Length; index++){
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index)) {
+                state = nextStates[index];
+                printStory();
+            }
+        }
+        // let's us quit the game at any given moment
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Application.Quit();
+            Debug.Log("Escape is pressed");
+        }
+        /*
+        if (Input.GetKeyDown(KeyCode.Alpha1)){
+            state = nextStates[0];
+            printStory();
+        } else if (Input.GetKeyDown(KeyCode.Alpha2)){
+            state = nextStates[1];
+            printStory();
+        }
+        */
+    }
+
+    private void printStory(){
+        storyTitleComponent.text = state.GetStoryTitle();
+        storyTextComponent.text = state.GetStateStory();
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject{
+    [SerializeField] string storyTitle;
+    [TextArea(10, 14)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+
+
+    public string GetStoryTitle() {
+        return storyTitle;
+    }
+
+    public string GetStateStory(){
+        return storyText;
+    }
+
+    public State[] GetNextStates(){
+        return nextStates;
+    }
+}
